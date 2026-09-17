30c5c83503fd86f6f331e4e088d13a8cc319d9e7
infinage <37257700+Infinage@users.noreply.github.com>
2026-09-17 19:38:29 +0530

    Contact form

--- internal/handlers/home_handler.go
+++ internal/handlers/home_handler.go
@@ -5,10 +5,11 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"strings"
 
 	"github.com/infinage/ecom-trading/internal/models"
+	"github.com/starfederation/datastar-go/datastar"
 )
 
 type App struct {
@@ -46,31 +47,69 @@ func NewApp(dbpath string, assets embed.FS, seedDB bool) (*App, error) {
 func (app *App) Routes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", app.handleHome)
+	mux.HandleFunc("GET /api/header/home", app.handlerAPIHome(false))
+	mux.HandleFunc("GET /api/header/products", app.handlerAPIHome(true))
+	mux.HandleFunc("GET /api/header/contact", app.handleAPIContact)
 	mux.Handle("GET /assets/", http.FileServerFS(app.assets))
 	return mux
 }
 
 func (app *App) handleHome(w http.ResponseWriter, r *http.Request) {
-	products, err := app.st.GetAllProducts(r.Context())
+	err := app.templ.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		errMsg := fmt.Sprintf("Execute template fail: %v\n", err)
+		http.Error(w, errMsg, http.StatusInternalServerError)
 		return
-	}
-
-	// Get all the unique categories
-	categorySt := make(map[string]struct{})
-	var categories []string
-	for _, p := range products {
-		if _, ok := categorySt[string(p.Category)]; !ok {
-			categorySt[string(p.Category)] = struct{}{}
-			categories = append(categories, string(p.Category))
-		}
-	}
-
-	data := map[string]any {"Products": products, "Categories": categories}
-	err = app.templ.ExecuteTemplate(w, "index.html", data)
-	if err != nil {
-		log.Printf("Execute template fail: %v\n", err)
 	}
 }
 
+func (app *App) handlerAPIHome(productsOnly bool) func(w http.ResponseWriter, r *http.Request)  {
+	return func(w http.ResponseWriter, r *http.Request) {
+		products, err := app.st.GetAllProducts(r.Context())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Get all the unique categories
+		categorySt := make(map[string]struct{})
+		var categories []string
+		for _, p := range products {
+			if _, ok := categorySt[string(p.Category)]; !ok {
+				categorySt[string(p.Category)] = struct{}{}
+				categories = append(categories, string(p.Category))
+			}
+		}
+
+		data := map[string]any {
+			"Products": products, 
+			"Categories": categories,
+			"ShowHero": strings.HasSuffix(r.URL.Path, "/home"),
+		}
+
+		// Build the home template string into buffer 
+		var buffer strings.Builder
+		err = app.templ.ExecuteTemplate(&buffer, "Home", data)
+		if err != nil {
+			errMsg := fmt.Sprintf("Execute template fail: %v\n", err)
+			http.Error(w, errMsg, http.StatusInternalServerError)
+			return
+		}
+
+		sse := datastar.NewSSE(w, r)
+		sse.PatchElements(buffer.String())
+	}
+}
+
+func (app *App) handleAPIContact(w http.ResponseWriter, r *http.Request) {
+	var buffer strings.Builder	
+	err := app.templ.ExecuteTemplate(&buffer, "Contact", nil)
+	if err != nil {
+		errMsg := fmt.Sprintf("Execute template fail: %v\n", err)
+		http.Error(w, errMsg, http.StatusInternalServerError)
+		return
+	}
+
+	sse := datastar.NewSSE(w, r)
+	sse.PatchElements(buffer.String())
+}
